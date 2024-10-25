c544682e7d38a07dad24eeceda904ce0ebb3a7a8
nwood313 <161774016+nwood313@users.noreply.github.com>
2024-10-24 21:07:52 -0400

    progress code showing up need the remaining data and transaction-details

--- src/main/java/com/codercampus/Assignment11/service/TransactionService.java
+++ src/main/java/com/codercampus/Assignment11/service/TransactionService.java
@@ -1,4 +1,33 @@
 package com.codercampus.Assignment11.service;
 
+import com.codercampus.Assignment11.domain.Transaction;
+import com.codercampus.Assignment11.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+//Sort the transactions by date
 public class TransactionService {
+    @Autowired
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> getAllTransactions(){
+        return transactionRepository.findAll().stream()
+                                              .sorted(Comparator.comparing(Transaction::getDate))
+                                              .collect(Collectors.toList());
+    }
+    public Transaction getTransactionById(Long transactionId) {
+        return transactionRepository.findAll().stream()
+                .filter(transaction -> transaction.getId().equals(transactionId))
+                .findFirst()
+                .orElse(null);
+    }
 }
--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -1,4 +1,38 @@
 package com.codercampus.Assignment11.web;
 
+import com.codercampus.Assignment11.domain.Transaction;
+import com.codercampus.Assignment11.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
 public class TransactionController {
+
+    @Autowired
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/transactions")
+    public String getAllTransactions(Model model) {
+        List<Transaction>transactions = transactionService.getAllTransactions();
+        model.addAttribute("transactions", transactions);
+        return "transactions"; //for the URL
+    }
+
+    @PostMapping("/transactions{transactionId}")
+    public String getTransactionDetails(@PathVariable Long transactionId, Model model) {
+        Transaction transaction = transactionService.getTransactionById(transactionId);
+        model.addAttribute("transaction", transaction);
+        return "transactions-details"; //
+
+    }
 }
