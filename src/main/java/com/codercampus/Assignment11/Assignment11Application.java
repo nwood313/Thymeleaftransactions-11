69a60ec739e518c53993df37a52655f96fee4e9f
nwood313 <nwood313@gmail.com>
2024-11-05 21:01:10 -0500

    accounting for user misdirection in html

--- src/main/java/com/codercampus/Assignment11/Assignment11Application.java
+++ src/main/java/com/codercampus/Assignment11/Assignment11Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Assignment11Application {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		SpringApplication.run(Assignment11Application.class, args);
-	}
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        SpringApplication.run(Assignment11Application.class, args);
+    }
 }
